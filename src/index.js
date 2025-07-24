096f24039df8f314a8ab108604d5cb373f97a9e4
haranHD <hariharan.ad22@bitsathy.ac.in>
2025-07-24 22:46:59 +0530

    React-new

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+function Java() {
+  return (
+    <h2>Java Developer</h2>
+  );
+}
+
+function Name() {
+  return (
+    <>
+      <h1>HARI HARAN</h1>
+      <Java/>
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Name />
   </React.StrictMode>
 );
 
