cbd84b3ec9c53d88085541b8647e6d1bab47fad9
haranHD <hariharan.ad22@bitsathy.ac.in>
2025-07-29 14:43:27 +0530

    update_state in object

--- src/components/Scooter.js
+++ src/components/Scooter.js
@@ -2,15 +2,30 @@ import { useState } from "react";
 
 function Scooter(){
 
-    const [color , setColor] = useState('Blue');
-    const [brand , setBrand] = useState('Honda');
-    const [model , setModel] = useState('Activa 5g');
+    // const [color , setColor] = useState('Blue');
+    // const [brand , setBrand] = useState('Honda');
+    // const [model , setModel] = useState('Activa 5g');
+
+    const [scooter , setScooter] = useState(
+        {
+            color : "Black",
+            brand : "TVs",
+            model : "6G"
+        }
+    );
+
+    function updateBrand(){
+        return setScooter(prev => {
+            return {...prev , brand : "Honda"}
+        });   
+    }
 
     return <>
         <h1>My Scooter</h1>
-        <p>Color : {color}</p>
-        <p>Brand : {brand}</p>
-        <p>Model : {model}</p>
+        <p>Color : {scooter.color}</p>
+        <p>Brand : {scooter.brand}</p>
+        <p>Model : {scooter.model}</p>
+        <button onClick={updateBrand}>Change Brand</button>
     </>
 }
 export default Scooter;
