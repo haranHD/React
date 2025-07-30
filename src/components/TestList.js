7486f825a776538f4a54dd0e5e01bc836e2d38a8
haranHD <hariharan.ad22@bitsathy.ac.in>
2025-07-30 18:11:08 +0530

    getting input

--- src/components/TestList.js
+++ src/components/TestList.js
@@ -2,14 +2,22 @@ import { useState } from "react"
 
 function List(){
 
-    const [list , setList] = useState(['Test1','Test2']);
+    const [list , setList] = useState([]);
+    const [count , setCount] = useState(1);
+
+    function addItem(){
+        const itemName = 'List ' + count;
+        setList((prev) => { return [...prev, itemName] })
+        setCount((prev) => {return prev + 1})
+    }
 
     return <>
         <h1>New List</h1>
+        <button onClick={addItem}> ADD LIST </button>
         <ul>
-                {
-                list.map((val , index) => <li key ={index}> {val}</li>)
-                }
+            {
+                list.map((val , index) => <li key = {index}>{val}</li>)
+            }
         </ul>
     </>
 }
