df482347230eba5fc2bc6c52616cdb74878e0679
haranHD <hariharan.ad22@bitsathy.ac.in>
2025-07-31 15:18:03 +0530

    just creacted the form

---
+++ src/components/Myform.js
@@ -0,0 +1,8 @@
+function Myform(){
+    return <>
+        <form action="text"><label htmlFor="">Enter your Name : <input type="text" /></label>
+            
+        </form>
+    </>
+}
+export default Myform;
